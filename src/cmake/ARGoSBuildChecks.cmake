8b37f9f0afa1255e25ebc829bde4ec8efb1b686c
iQuad427 <quentin.roels@gmail.com>
2024-01-20 11:58:57 +0100

    added argos3-beta59 compatibility

--- src/cmake/ARGoSBuildChecks.cmake
+++ src/cmake/ARGoSBuildChecks.cmake
@@ -1,19 +1,29 @@
 #
+# Check ARGoS
+#
+find_package(ARGoS REQUIRED)
+include_directories(${ARGOS_INCLUDE_DIRS})
+link_directories(${ARGOS_LIBRARY_DIR})
+link_libraries(${ARGOS_LDFLAGS})
+string(REPLACE "/lib/argos3" "" ARGOS_PREFIX "${ARGOS_LIBRARY_DIR}")
+set(CMAKE_INSTALL_PREFIX ${ARGOS_PREFIX} CACHE STRING "Install path prefix, prepended onto install directories." FORCE)
+
+#
 # Find the ARGoS package
 #
-if(ARGOS_BUILD_FOR_SIMULATOR)
-  find_package(PkgConfig)
-  pkg_check_modules(ARGOS REQUIRED argos3_simulator)
-  set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${ARGOS_PREFIX}/share/argos3/cmake)
-  include_directories(${ARGOS_INCLUDE_DIRS})
-  link_directories(${ARGOS_LIBRARY_DIRS})
-endif(ARGOS_BUILD_FOR_SIMULATOR)
+#if(ARGOS_BUILD_FOR_SIMULATOR)
+#  find_package(PkgConfig)
+#  pkg_check_modules(ARGOS REQUIRED argos3_simulator)
+#  set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${ARGOS_PREFIX}/share/argos3/cmake)
+#  include_directories(${ARGOS_INCLUDE_DIRS})
+#  link_directories(${ARGOS_LIBRARY_DIRS})
+#endif(ARGOS_BUILD_FOR_SIMULATOR)
 
 #
 # Check for Qt and OpenGL when compiling for the simulator
 #
 if(ARGOS_BUILD_FOR_SIMULATOR)
-  include(ARGoSCheckQTOpenGL)
+include(FindARGoSQTOpenGL)
 endif(ARGOS_BUILD_FOR_SIMULATOR)
 
 #
