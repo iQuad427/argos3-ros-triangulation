8f98cf2cd1ac822ce95bff6a4c589a89de2fd717
iQuad427 <quentin.roels@gmail.com>
2024-03-29 12:08:26 +0000

    implementation for beta 59

--- src/cmake/ARGoSBuildChecks.cmake
+++ src/cmake/ARGoSBuildChecks.cmake
@@ -9,17 +9,6 @@ string(REPLACE "/lib/argos3" "" ARGOS_PREFIX "${ARGOS_LIBRARY_DIR}")
 set(CMAKE_INSTALL_PREFIX ${ARGOS_PREFIX} CACHE STRING "Install path prefix, prepended onto install directories." FORCE)
 
 #
-# Find the ARGoS package
-#
-#if(ARGOS_BUILD_FOR_SIMULATOR)
-#  find_package(PkgConfig)
-#  pkg_check_modules(ARGOS REQUIRED argos3_simulator)
-#  set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${ARGOS_PREFIX}/share/argos3/cmake)
-#  include_directories(${ARGOS_INCLUDE_DIRS})
-#  link_directories(${ARGOS_LIBRARY_DIRS})
-#endif(ARGOS_BUILD_FOR_SIMULATOR)
-
-#
 # Check for Qt and OpenGL when compiling for the simulator
 #
 if(ARGOS_BUILD_FOR_SIMULATOR)
